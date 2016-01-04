7a1b68955065dcb26c0fecf36d0af9c993531115
Ben Smithgall <ben.smithgall@gmail.com>
2016-01-04 00:34:53 -0500

    add marshmallow expiriment for schema, pipeline
    This commit adds a depedency on Marshmallow[1], a lightweight serialization/deserialization library that can also handle validation, requiring fields, etc. Additionally, I have made an attempt to set up simple pipeline building through method chaining. For example, building a pipeline might look like this:
    
    my_pipeline = Pipeline().extractor(CsvExractor).schema(MySchema).load(Datapusher)
    
    This will instantiate a new Pipeline, which would then be able to be either scheduled or run as expected. That functionality is to be added later. I've added a sample job taken from the Fatal OD in the existing WPRDC codebase for an example of what this would look like (and a skeleton test for it as well).
    
    [1] http://marshmallow.readthedocs.org/en/latest/

---
+++ jobs/fatal_od.py
@@ -0,0 +1,42 @@
+import datetime
+from marshmallow import fields, post_load
+
+import pipeline as pl
+
+class FatalODSchema(pl.BaseSchema):
+    death_date = fields.DateTime(format='%m/%d/%Y')
+    death_time = fields.DateTime(format='%I:%M %p')
+    manner_of_death = fields.String()
+    age = fields.Integer()
+    sex = fields.String()
+    race = fields.String()
+    case_dispo = fields.String()
+    combined_od1 = fields.String()
+    combined_od2 = fields.String()
+    combined_od3 = fields.String()
+    combined_od4 = fields.String()
+    combined_od5 = fields.String()
+    combined_od6 = fields.String()
+    combined_od7 = fields.String()
+    incident_zip = fields.Integer()
+    decedent_zip = fields.Integer()
+    case_year = fields.Integer()
+
+    @post_load
+    def combine_date_and_time(self, in_data):
+        death_date, death_time = in_data['death_date'], in_data['death_time']
+        in_data['death_date_and_time'] = datetime.datetime(
+            death_date.year, death_date.month, death_date.day,
+            death_time.hour, death_time.minute, death_time.second
+        )
+
+    @post_load
+    def replace_death_time_date(self, in_data):
+        today = datetime.datetime.today()
+        in_data['death_time'].replace(
+            year=today.year, month=today.month, day=today.day
+        )
+
+fatal_od_pipeline = pl.Pipeline().extract(pl.CSVExtractor, firstline=True) \
+    .schema(FatalODSchema) \
+    .load(pl.Datapusher)
