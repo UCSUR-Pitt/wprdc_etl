a54db238d86ec5a848ee70d43fd31346a6051875
saylorsd <steveosaylor@gmail.com>
2016-02-18 15:47:19 -0500

    added test for asbestos permit job

---
+++ test/jobs/test_asbestos_permits_pipeline.py
@@ -0,0 +1,23 @@
+import os
+
+from jobs.asbestos_permits import AsbestosPermitSchema
+import pipeline as pl
+
+from .base import TestBase, HERE
+
+class TestODPipeline(TestBase):
+    def test_permits_pipeline(self):
+        pl.Pipeline(
+            'asbestos_permits_pipeline', 'Asbestos Permit Pipeline',
+            settings_file=self.settings_file,
+            conn=self.conn
+        ) \
+            .connect(pl.FileConnector, os.path.join(HERE, '../mock/asbestos_mock.xlsx'), encoding=None) \
+            .extract(pl.ExcelExtractor, firstline_headers=True) \
+            .schema(AsbestosPermitSchema) \
+            .load(self.Loader) \
+            .run()
+        status = self.cur.execute('select * from status').fetchall()
+        self.assertEquals(len(status), 1)
+        self.assertEquals(status[0][-2], 'success')
+        self.assertEquals(status[0][-1], 1)
