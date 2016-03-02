da3383e802d78c6e39308080e8281008c0e1d1f2
saylorsd <steveosaylor@gmail.com>
2016-03-02 11:39:53 -0500

    added test of schema.dumps functionality to test_jail_census

--- test/jobs/test_jail_census_pipeline.py
+++ test/jobs/test_jail_census_pipeline.py
@@ -3,11 +3,14 @@ import os
 from jobs.jail_census import JailCensusSchema
 import pipeline as pl
 
+from collections import OrderedDict
+
 from .base import TestBase, HERE
+
 
 class TestJailPipeline(TestBase):
     def test_jail_pipeline(self):
-        pl.Pipeline(
+        jail_test_pipeline = pl.Pipeline(
             'jail_census_combined_pipeline', 'Jail Census Combined Pipeline',
             settings_file=self.settings_file,
             conn=self.conn
@@ -19,6 +22,16 @@ class TestJailPipeline(TestBase):
             .run()
         status = self.cur.execute('select * from status').fetchall()
 
+        self.assertEquals(
+            jail_test_pipeline.data[0],
+            OrderedDict(
+                (('Date', '2016-01-03'),
+                 ('Gender', 'M'),
+                 ('Race', 'B'),
+                 ('Age at Booking', '24'),
+                 ('Current Age', '26'))
+            )
+        )
         self.assertEquals(len(status), 1)
         self.assertEquals(status[0][-2], 'success')
         self.assertEquals(status[0][-1], 17)
